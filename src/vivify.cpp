0f335b3aa06406311d781f9c2f5e30e7752b825b
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2025-11-25 14:17:06 +0100

    fix some warnings (#149)

--- src/vivify.cpp
+++ src/vivify.cpp
@@ -814,8 +814,7 @@ inline void Internal::vivify_increment_stats (const Vivifier &vivifier) {
   case Vivify_Mode::TIER3:
     ++stats.vivifystred3;
     break;
-  default:
-    assert (vivifier.tier == Vivify_Mode::IRREDUNDANT);
+  case Vivify_Mode::IRREDUNDANT:
     ++stats.vivifystrirr;
     break;
   }
@@ -1176,8 +1175,7 @@ bool Internal::vivify_clause (Vivifier &vivifier, Clause *c) {
     case Vivify_Mode::TIER2:
       ++stats.vivifiedtier2;
       break;
-    default:
-      assert (vivifier.tier == Vivify_Mode::TIER3);
+    case Vivify_Mode::TIER3:
       ++stats.vivifiedtier3;
       break;
     }
@@ -1455,7 +1453,7 @@ inline std::vector<Clause *> &current_schedule (Vivifier &vivifier) {
   case Vivify_Mode::TIER3:
     return vivifier.schedule_tier3;
     break;
-  default:
+  case Vivify_Mode::IRREDUNDANT:
     return vivifier.schedule_irred;
     break;
   }
@@ -1709,8 +1707,7 @@ void set_vivifier_mode (Vivifier &vivifier, Vivify_Mode tier) {
   case Vivify_Mode::TIER3:
     vivifier.tag = 'w';
     break;
-  default:
-    assert (tier == Vivify_Mode::IRREDUNDANT);
+  case Vivify_Mode::IRREDUNDANT:
     vivifier.tag = 'x';
     break;
   }
